6f86e441059607bd2ee350ca38a95e7bf6157a6e
Ruslan <ruslan.mirets@gmail.com>
2022-11-30 20:53:01 +0300

    Building the Hero Section (1/2)

---
+++ components/Hero.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const Hero: FC = () => {
+	return <div>Hero</div>;
+};
+
+export default Hero;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import Hero from "../components/Hero";
 
 export default function Home() {
 	return (
@@ -10,7 +11,9 @@ export default function Home() {
 			<Header />
 
 			{/* Hero */}
-
+			<section id="hero">
+				<Hero />
+			</section>
 			{/* About */}
 
 			{/* Experience */}
