a9aa13a05502adb85cdf998a3643fff19e7ca7b1
Ruslan <ruslan.mirets@gmail.com>
2022-11-30 17:00:34 +0300

    Setting up Next.js & TypeScript

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>Sonny&apos;s Portfolio</title>
+			</Head>
+		</div>
+	);
+}
