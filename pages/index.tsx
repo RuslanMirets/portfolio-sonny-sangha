c3775831cb4a58bf6e4929d478514792f58950c6
Ruslan <ruslan.mirets@gmail.com>
2022-11-30 17:21:03 +0300

    Setting up Tailwind CSS

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
 	return (
-		<div className={styles.container}>
+		<div>
 			<Head>
 				<title>Sonny&apos;s Portfolio</title>
 			</Head>
+			<h1 className="text-red-500">Hello</h1>
 		</div>
 	);
 }
