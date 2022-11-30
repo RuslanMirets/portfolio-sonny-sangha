83e8c54c65cf4ab770f7709f8e59f94dace8e580
Ruslan <ruslan.mirets@gmail.com>
2022-11-30 21:09:35 +0300

    Implementing React Simple Typewriter Library

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,21 @@
 import { FC } from "react";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
 
 const Hero: FC = () => {
-	return <div>Hero</div>;
+	const [text, count] = useTypewriter({
+		words: ["Developer", "Designer", "Creator"],
+		loop: true,
+		delaySpeed: 2000,
+	});
+
+	return (
+		<div>
+			<h1>
+				<span>{text}</span>
+				<Cursor cursorColor="#f7ab0a" />
+			</h1>
+		</div>
+	);
 };
 
 export default Hero;
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Hero from "../components/Hero";
 
 export default function Home() {
 	return (
-		<div>
+		<div className="bg-[rgb(36,36,36)] text-white h-screen">
 			<Head>
 				<title>Sonny&apos;s Portfolio</title>
 			</Head>
