bf6fb49eff51938492f5b3fc91791f0cf972d0ac
Ruslan <ruslan.mirets@gmail.com>
2022-12-01 10:39:15 +0300

    Building the Hero Section (2/2)

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import Link from "next/link";
 import { FC } from "react";
 import { useTypewriter, Cursor } from "react-simple-typewriter";
 import BackgroundCircles from "./BackgroundCircles";
@@ -12,10 +14,36 @@ const Hero: FC = () => {
 	return (
 		<div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
 			<BackgroundCircles />
-			<h1>
-				<span>{text}</span>
-				<Cursor cursorColor="#f7ab0a" />
-			</h1>
+			<Image
+				className="relative rounded-full w-32 h-32 mx-auto object-cover"
+				src="https://coolsen.ru/wp-content/uploads/2021/09/249.jpg"
+				width={128}
+				height={128}
+				alt="Avatar"
+			/>
+			<div className="z-20">
+				<h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+					Software Engineer
+				</h2>
+				<h1 className="text-5xl lg:text-6xl font-semibold px-10">
+					<span className="mr-3">{text}</span>
+					<Cursor cursorColor="#f7ab0a" />
+				</h1>
+				<div className="pt-5">
+					<Link href="#about">
+						<button className="heroButton">About</button>
+					</Link>
+					<Link href="#experience">
+						<button className="heroButton">Experience</button>
+					</Link>
+					<Link href="#skills">
+						<button className="heroButton">Skills</button>
+					</Link>
+					<Link href="#projects">
+						<button className="heroButton">Projects</button>
+					</Link>
+				</div>
+			</div>
 		</div>
 	);
 };
