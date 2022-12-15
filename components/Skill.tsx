0103c812fd0299fbaf32368eae0b7374ac34d03d
Ruslan <ruslan.mirets@gmail.com>
2022-12-15 11:07:44 +0300

    Building the Skill Component in the Skills Section

---
+++ components/Skill.tsx
@@ -0,0 +1,28 @@
+import { motion } from "framer-motion";
+import { FC } from "react";
+
+interface IProps {
+	directionLeft?: boolean;
+}
+
+const Skill: FC<IProps> = ({ directionLeft }) => {
+	return (
+		<div className="group relative flex cursor-pointer">
+			<motion.img
+				className="rounded-full border border-gray-500 object-cover w-24 h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
+				initial={{ x: directionLeft ? -200 : 200, opacity: 0 }}
+				whileInView={{ x: 0, opacity: 1 }}
+				transition={{ duration: 1 }}
+				src="https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_96102ac6497377cd53da621075fe828e/sanity.png"
+				alt="Icon"
+			/>
+			<div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white w-24 h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0">
+				<div className="flex items-center justify-center h-full">
+					<p className="text-3xl font-bold text-black opacity-100">100%</p>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Skill;
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,9 +1,15 @@
 import { motion } from "framer-motion";
 import { FC } from "react";
+import Skill from "./Skill";
 
 const Skills: FC = () => {
 	return (
-		<motion.div className="flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center">
+		<motion.div
+			className="flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center"
+			initial={{ opacity: 0 }}
+			whileInView={{ opacity: 1 }}
+			transition={{ duration: 1.5 }}
+		>
 			<h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
 				Skills
 			</h3>
@@ -13,6 +19,19 @@ const Skills: FC = () => {
 			</h3>
 
 			<div className="grid grid-cols-4 gap-5">
+				<Skill />
+				<Skill />
+				<Skill />
+				<Skill />
+				<Skill />
+				<Skill />
+				<Skill />
+				<Skill />
+				<Skill />
+				<Skill />
+				<Skill />
+				<Skill />
+				<Skill />
 				<Skill />
 			</div>
 		</motion.div>
